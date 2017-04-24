fbaaa93b22d901506e712f66a055d889ad57ba5d
Pisarik <mr_plum@mail.ru>
2017-04-24 03:24:24 +0300

    check for undefined callbacks, break after find and updateCardById

--- storageManager.js
+++ storageManager.js
@@ -1,5 +1,3 @@
-//let storage = chrome.storage.local;
-
 class StorageManager{
 	constructor(){
 	}
@@ -10,37 +8,59 @@ class StorageManager{
 
 	// async, therefore calls callback = function(card)
 	static getCardById(id, callback){
-		
 		StorageManager.STORAGE.get({'cards': []}, function(result){
 			let cards = result.cards;
 
 			for (let i = 0; i < cards.length; i++){
 				if (cards[i].id === id){
 					callback(cards[i]);
+					break;
 				}
 			}
 		});
+	}
 
+	// async, therefore calls callback = function(isUpdated)
+	static updateCardById(id, newCard, callback){
+		StorageManager.STORAGE.get({'cards': []}, function(result){
+			let cards = result.cards;
+			let isUpdated = false;
+
+			for (let i = 0; i < cards.length; i++){
+				if (cards[i].id === id){
+					cards[i] = newCard
+
+					StorageManager.STORAGE.set({'cards': cards});
+					isUpdated = true;
+
+					break;
+				}
+			}
+
+			if (callback){
+				callback(isUpdated);
+			}
+		});
 	}
 
 	// async, therefore calls callback = function(isAdded)
 	static addCard(card, callback){
 		StorageManager.STORAGE.get({'cards': []}, function(result){
 			let cards = result.cards;
+			let exist = isCardExist(cards, card);
 
-			if (!isCardExist(cards, card)){
+			if (!exist){
 				cards.push(card);
 
-				StorageManager.STORAGE.set({'cards': cards}, function(){
-					StorageManager.STORAGE.get({'cards': []}, function(result){
-						console.log("New cards: " + result.cards);
-					});
-				});
-				callback(true);
+				StorageManager.STORAGE.set({'cards': cards});
 			}
 			else{
 				console.log("This card already exists: " + card);
-				callback(false);
+			}
+
+			if (callback){
+				let isAdded = !exist;
+				callback(isAdded);
 			}
 		});
 	}
